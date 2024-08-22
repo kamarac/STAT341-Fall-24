cad1c333a5da2246e6e7b9c3209665e9f13c30a7
kamarac <137800578+kamarac@users.noreply.github.com>
2024-08-22 15:32:41 -0500

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,28 @@
+#Author: Kamara, 08/22/24, Purpose: Test the measures of descriptive stats
+
+# name () = function 
+# run code in console , if quit, anything ran is deleted 
+
+# need data / input for a function
+
+X <- c(1,2,3,4,5)
+
+# we can append x & add 6 by writing: X<-6
+# want to compartmentilize material on right side into X (variable)
+
+Y <- 100
+
+#though, if more than 1 #, use container / c 
+
+#apply a fucntion for SD to calculate SD (of material in X):
+
+# How to calculate standard deviation in R 
+sd(X)
+
+#  How to calculate mean in R
+mean(X)
+
+# How to calculate median in R 
+Median(X)
+
+#mode is not present in R language, though we can write own 
